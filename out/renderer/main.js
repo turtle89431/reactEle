26f0bb9f43f6566eab32e5275c0bbcb5b7afcd48
Walter Winser <turtle89431@gmail.com>
2019-06-06 18:44:34 -0700

    ready to use

---
+++ out/renderer/main.js
@@ -0,0 +1,6 @@
+//start react app that starts in gui/app.js
+/*
+        Main.setState({name:"state updated"}) - example how to update state from outside react
+        Main.state - view state outside react
+      */
+Main.run();
